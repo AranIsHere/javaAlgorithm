b9dd58460fcc7ae251bafcaee5379ca5fcf8ab8d
AranIsHere <343081716@qq.com>
2020-05-19 17:32:37 +0800

    加入原作者文章链接

--- basicPractice/MonkeyTakePeach.java
+++ basicPractice/MonkeyTakePeach.java
@@ -5,8 +5,10 @@ package basicPractice;
  * 海滩上有一堆桃子，五只猴子来分，第一只猴子把这堆桃子平均分成五份，多了一个，这只猴子把多的一个
  * 扔进海中，拿走一份。第二只猴子把剩下的桃子又平均分成五份，又多了一个，它同样把多的一个扔进海中，拿走一份，第三、
  * 第四、第五只猴子都是这样做的，问海滩上原来有多少个桃子？
- * 
+ *  
  * @author 秋夫人
+ * 
+ * 借鉴上述作者文章，原文地址：https://blog.csdn.net/wangnanwlw/article/details/52488132
  */
 public class MonkeyTakePeach {
     public static void main(String[] args){
